604b77d1107e28398818c7792dfc595b2103dbc5
Vishv Joshi <201901453@daiict.ac.in>
2021-01-30 22:00:48 +0530

    event registration and participant registration almost complete

--- Event Manager/Event_Manager/settings.py
+++ Event Manager/Event_Manager/settings.py
@@ -118,5 +118,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'vishvjoshi2001@gmail.com'
-EMAIL_HOST_PASSWORD = 'rubickscube'
+EMAIL_HOST_USER = ''     # put your own gmail credentials
+EMAIL_HOST_PASSWORD = ''
